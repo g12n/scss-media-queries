ded66e1d4f1e425b75a8f1f0c2c87c54d7bff5bb
Michael Gehrmann <michael.gehrmann@gmail.com>
2014-07-03 16:05:49 +0200

    Initial Commit
    First Commit
    
    Signed-off-by: Michael Gehrmann <michael.gehrmann@gmail.com>

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+
+
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    sass: {
+      dist: {
+        options: {
+              style: 'compressed', 
+              compass: true
+        },
+        files: {
+              'build/css/mediaqueries.css': 'src/sass/mediaqueries.scss'
+        }
+      }
+    },
+  watch: {
+    css: {
+        files: ['src/**/*.scss'],
+        tasks: ['sass']
+    },
+    html:{
+      files: ['src/*.html'],
+      tasks: ['copy:html']
+
+    }
+  },  
+  copy:{
+    html:{
+       files: [{expand: true, cwd: 'src/', src: ['*.html'], dest: 'build/'}]
+    }
+  }
+});
+
+
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+
+
+  // default Task for compiling SCSS and build docu (cross browser test)
+  grunt.registerTask('default', ['sass', 'copy','watch']);
+  
+};
---
+++ readme.md
@@ -0,0 +1,13 @@
+## Media Queries for Standard Devices and Categories
+
+The goal of this project is to provide an easy test media queries for different standart devices and constantly update a set of standard queries for everyday use in CSS. The Queries are based on [this snippet on css-ticks.com][media-queries-snippets] provided by [Chris Coyier][link-chris].
+
+I Borrow a technique discribed by [Mason Wendell][link-mason] in [this article][link-responsive-sass] by.
+
+
+[media-queries-snippets]:http://css-tricks.com/snippets/css/media-queries-for-standard-devices/ "Media Queries for Standard Devices"
+
+[link-responsive-sass]: http://thesassway.com/intermediate/responsive-web-design-in-sass-using-media-queries-in-sass-32 "Responsive Web Design in Sass: Using media queries in Sass 3.2"
+
+[link-chris]:https://twitter.com/chriscoyier "Chris Coyier"
+[link-mason]:https://twitter.com/codingdesigner "Mason Wendell"
